6c999fe4fc97c742d2fe338d46278a1c46e4e309
Gary Mc Dermott <gary.mcdermott@cloudreach.com>
2019-02-13 23:09:38 -0800

    changed profile to personalAWS A/c

---
+++ S3SLSWebsite/variables.tf
@@ -0,0 +1,10 @@
+variable "profile" {}
+variable "region" {}
+
+variable "Tag_Name" {}
+variable "Tag_Stage" {}
+variable "Tag_Owner" {}
+variable "Tag_Monitored" {}
+
+variable "bucket_1" {}
+variable "bucket_2" {}
