d1b30c294fea14c2c61168c1114e61893640861a
mcdermg <mcdermg@tcd.ie>
2019-05-20 19:11:09 -0300

    Updating RDS

--- RDS/rds.tf
+++ RDS/rds.tf
@@ -14,39 +14,39 @@ resource "aws_db_instance" "default" {
   password             = "${var.password}"
   parameter_group_name = "default.mysql5.7"
   identifier           = "${var.identifier}"
-
-  # No final snapshot when using Terraform destroy
-  skip_final_snapshot = "True"
-
-  output "RDS_Host" {
-    value = "${aws_db_instance.default.address}"
-  }
-
-  output "RDS_User" {
-    value = "${aws_db_instance.default.username}"
-  }
-
-  output "RDS_PW" {
-    value = "${aws_db_instance.default.password}"
-  }
-
-  output "RDS_DB_Name" {
-    value = "${aws_db_instance.default.name}"
-  }
+  skip_final_snapshot  = "True"                     # No final snapshot when using Terraform destroy
+  publicly_accessible  = "True"                     # Publicly accessable via internet
 
   tags {
     Name  = "${var.Tag_Name}"
     Stage = "${var.Tag_Stage}"
     Owner = "${var.Tag_Owner}"
   }
-  provisioner "local-exec" {
-    command = "echo #!/usr/bin/env python
-    import preprocessing
 
-    mysql = {'host': '${aws_db_instance.default.address}',
-             'user': '${aws_db_instance.default.username}',
-             'password': '${aws_db_instance.default.password}',
-             'db': '${aws_db_instance.default.name}'
-             } > databaseconfig.py"
-  }
+  #  provisioner "local-exec" {
+  #    command = "echo #!/usr/bin/env python
+  #    import preprocessing
+  #
+  #    mysql = {'host': '${aws_db_instance.default.address}',
+  #             'user': '${aws_db_instance.default.username}',
+  #             'password': '${aws_db_instance.default.password}',
+  #             'db': '${aws_db_instance.default.name}'
+  #             } > databaseconfig.py"
+  #  }
+}
+
+output "RDS_Host" {
+  value = "${aws_db_instance.default.address}"
+}
+
+output "RDS_User" {
+  value = "${aws_db_instance.default.username}"
+}
+
+output "RDS_PW" {
+  value = "${aws_db_instance.default.password}"
+}
+
+output "RDS_DB_Name" {
+  value = "${aws_db_instance.default.name}"
 }
