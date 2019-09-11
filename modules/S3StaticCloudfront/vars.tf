09cd64a1c8e93ca93ef1f0378c711c35f9848992
Gary Mc Dermott <mcdermg@tcd.ie>
2019-09-10 22:00:54 -0300

    Added region var to module

--- modules/S3StaticCloudfront/vars.tf
+++ modules/S3StaticCloudfront/vars.tf
@@ -6,3 +6,7 @@ variable "certificate_arn" {
   description = "AWS acm certificate ARN. Due to manual reuirment with verify not automating"
   default = "arn:aws:acm:us-east-1:949207431024:certificate/f88d984e-b3c9-4ae9-a3f6-9deb4a369d48" 
 }
+
+variable "region" {
+  default = "ca-central-1"
+}
